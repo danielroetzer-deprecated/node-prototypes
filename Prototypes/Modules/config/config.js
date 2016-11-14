b7b601af7c06cc7a0d039054f213692e8c0e437b
Daniel Rötzer <roetzer.daniel@gmail.com>
2016-11-14 16:34:29 +0100

    Prototypes: BodyParser, Modules, Morgan, RethinkDb updated

--- Prototypes/Modules/config/config.js
+++ Prototypes/Modules/config/config.js
@@ -2,22 +2,32 @@
  * Created by Dani on 13.11.2016.
  */
 
-//Exporting 4 different selfmade modules
+//Exporting various modules in different ways
 module.exports = {
     firstFunction : function () {
         return "Return of the first function";
     },secondFunction: function () {
         return "Return of the second function";
-    },thirdfunction: function(number1,number2){
-        var result = number1*number2;
-        return "" + number1 + " * " + number2 + " = " + result;
+    },server: {
+        "host":"localhost",
+        "port":3000
     },values: {
         "name":"Daniel",
         "location":"Melk",
         "number":"06603448790"
-    },hello
+    },
+    //This method of calling the functions is possible with
+    //the Javascript version: ECMAScript 6
+    sayHello,
+    sum
 };
 
-function hello(){
-    return "hello";
+
+function sum(number1,number2){
+    var result = number1+number2;
+    return "" + number1 + " + " + number2 + " = " + result;
+}
+
+function sayHello(){
+    return "Hello, fellow user";
 }
--- Prototypes/Modules/index.js
+++ Prototypes/Modules/index.js
@@ -2,14 +2,32 @@
  * Created by Dani on 13.11.2016.
  */
 
+var express = require('express');
+var app = express();
+
 //Importing the modules from the config file
 var config = require("./config/config");
 
 //Call the defined functions
 console.log(config.firstFunction());
 console.log(config.secondFunction());
-console.log(config.thirdfunction(3,5));
-console.log(config.hello());
+console.log(config.sayHello());
+//Build sum
+console.log(config.sum(3,5));
+
 //Call the defined values
 console.log(config.values);
 console.log(config.values.name);
+
+
+//This will display all available objects, which are loaded from the config
+app.get('/',function (req,res) {
+    res.send(config);
+})
+
+
+//Access the port with the loaded config
+app.listen(config.server.port, function () {
+    console.log('Server listening on port: '+config.server.port);
+})
+
