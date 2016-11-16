9a9659d09cb84a8f8e5c243fadac7e2920477ae0
Daniel Rötzer <roetzer.daniel@gmail.com>
2016-11-16 01:06:29 +0100

    Prototype RethinkDB updated

--- Prototypes/RethinkDB/index.js
+++ Prototypes/RethinkDB/index.js
@@ -3,25 +3,8 @@
  */
 
 
-//Load and start Express
-var express = require('express');
-var app = express();
-
-
-//Load config
-var config = require('./config/config.js');
-
 //Load Database file
-var db = require('./models/db.js');
+const db = require('./models/db.js');
 
 db.initDB();
-db.insertTestData();
-
-app.get('/',function (req, res) {
-    res.send('Main page');
-});
-
-
-app.listen(config.server.port, function () {
-    console.log('Listening on port: ' + config.server.port)
-});
+setTimeout(db.insertTestData, 2000);
